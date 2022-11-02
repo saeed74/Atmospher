5438627ae5085f433c4c4a91ce783e5b52171fb6
Saeed <saeed.dc2011@gmail.com>
2022-11-02 16:41:24 +0330

    App widget UI implemented

--- app/src/main/java/com/arianmanesh/atmospher/WeatherAppWidget.kt
+++ app/src/main/java/com/arianmanesh/atmospher/WeatherAppWidget.kt
@@ -4,6 +4,9 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.AppWidgetTarget
+
 
 /**
  * Implementation of App Widget functionality.
@@ -34,10 +37,20 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
+    //val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.weather_app_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    val city = "IRAN | TEST"
+    val temp = "20°C"
+    views.setTextViewText(R.id.txtCityName, city)
+    views.setTextViewText(R.id.txtCityTemperature, temp)
+
+    val appWidgetTarget = AppWidgetTarget(context,R.id.imgWeatherIcon,views,appWidgetId)
+
+    Glide.with(context.applicationContext)
+        .asBitmap()
+        .load("https://cdn.weatherapi.com/weather/64x64/night/302.png")
+        .into(appWidgetTarget)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
