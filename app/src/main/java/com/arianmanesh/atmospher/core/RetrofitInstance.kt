d0e226c7e38a4182f1f7e0f9fa985a8e70df0a01
Saeed <saeed.dc2011@gmail.com>
2022-11-03 11:54:28 +0330

    HttpClient 15sec timeout set

--- app/src/main/java/com/arianmanesh/atmospher/core/RetrofitInstance.kt
+++ app/src/main/java/com/arianmanesh/atmospher/core/RetrofitInstance.kt
@@ -1,13 +1,24 @@
 package com.arianmanesh.atmospher.core
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 
 object RetrofitInstance {
     val apiKey : String by lazy { "db7fff764dff4213a58103931220111" }
     val api : ApiService by lazy {
+
+        val okHttpClient = OkHttpClient().newBuilder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .build()
+
         Retrofit.Builder()
             .baseUrl("https://api.weatherapi.com/")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
