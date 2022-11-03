6f7d26ba5c3b4a16e51d7ddf998c879baaaa04de
Saeed <saeed.dc2011@gmail.com>
2022-11-03 11:15:06 +0330

    App widget fully implemented

--- app/src/main/java/com/arianmanesh/atmospher/WeatherAppWidget.kt
+++ app/src/main/java/com/arianmanesh/atmospher/WeatherAppWidget.kt
@@ -3,21 +3,25 @@ package com.arianmanesh.atmospher
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.util.Log
+import android.view.View
 import android.widget.RemoteViews
+import com.arianmanesh.atmospher.core.ResponseResult
+import com.arianmanesh.atmospher.core.RetrofitInstance
+import com.arianmanesh.atmospher.database.AtmosphereDataBase
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.AppWidgetTarget
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-
-/**
- * Implementation of App Widget functionality.
- */
 class WeatherAppWidget : AppWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
@@ -32,26 +36,76 @@ class WeatherAppWidget : AppWidgetProvider() {
     }
 }
 
+lateinit var atmosphereDB: AtmosphereDataBase
+
 internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    //val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
+
+    Log.e("TAG_W","Widget update started")
+
+    atmosphereDB = AtmosphereDataBase.getInstance(context)
+
     val views = RemoteViews(context.packageName, R.layout.weather_app_widget)
-    val city = "IRAN | TEST"
-    val temp = "20°C"
-    views.setTextViewText(R.id.txtCityName, city)
-    views.setTextViewText(R.id.txtCityTemperature, temp)
 
+    if(!atmosphereDB.citiesDao().isAnyCityAlreadySelected()){
+        views.setViewVisibility(R.id.lnlWidgetDataContainer, View.GONE)
+        views.setViewVisibility(R.id.txtWidgetStatus, View.VISIBLE)
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+        return
+    }
+
+    views.setViewVisibility(R.id.txtWidgetStatus, View.GONE)
+    views.setViewVisibility(R.id.prgLoading, View.VISIBLE)
+    val currentCity = atmosphereDB.citiesDao().getCurrentSelectedCity()
+
+    GlobalScope.launch(Dispatchers.IO) {
+
+        val response = RetrofitInstance.api.getWeatherDetail(RetrofitInstance.apiKey,currentCity.name)
+
+        withContext(Dispatchers.Main) {
+            views.setViewVisibility(R.id.prgLoading, View.GONE)
+            if (response.isSuccessful) {
+                val body = response.body()
+                if (body != null) {
+                    showResultOnWidget(views,
+                        context,
+                        appWidgetId,
+                        body.location.name,
+                        body.location.country,
+                        body.current.temp_c,
+                        body.current.condition.icon)
+                }
+            }else{
+                showResultOnWidget(views,
+                    context,
+                    appWidgetId,
+                    currentCity.name,
+                    currentCity.country,
+                    currentCity.temp_c,
+                    currentCity.icon)
+            }
+        }
+
+    }
+
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+private fun showResultOnWidget(views: RemoteViews, context: Context, appWidgetId: Int,  name: String, country: String, tempC: Long, icon: String) {
+
+    views.setTextViewText(R.id.txtCityName, buildCityText(country,name))
+    views.setTextViewText(R.id.txtCityTemperature, (tempC.toString() + context.getString(R.string.centigrade_sign)))
     val appWidgetTarget = AppWidgetTarget(context,R.id.imgWeatherIcon,views,appWidgetId)
-
     Glide.with(context.applicationContext)
         .asBitmap()
-        .load("https://cdn.weatherapi.com/weather/64x64/night/302.png")
+        .load("https:${icon}")
         .into(appWidgetTarget)
 
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+private fun buildCityText(country: String, city: String): String {
+    return (country + " | " + (city).lowercase().replaceFirstChar { it.uppercase() } )
 }
